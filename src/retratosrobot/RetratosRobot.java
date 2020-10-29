6bad86672f46ef8aeeadfb0234b2af3ab3b150d6
mfcr <manuel_campelo@yahoo.com>
2020-10-30 00:26:32 +0100

    Cambios en la V2.0. Usando un solo array para toda la cara

--- src/retratosrobot/RetratosRobot.java
+++ src/retratosrobot/RetratosRobot.java
@@ -7,43 +7,33 @@ public class RetratosRobot {
 	 */
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
+		//Nota importante, se puede crar un array bidimiensional con todos los rasgos faciales donde en la psocion 0 este el nombre del rasgo facial.
+		// a la hora de presentar los rasgos
 		//Definimos tipos.
-		String[] pelo= {"WWWWWWWWW","\\\\\\//////","|\"\"\"\"\"\"\"|","|||||||||"};
-		String[] ojos= {"| O   O |","|-(· ·)-|","|-(o o)-|","| \\   / |"};
-		String[] nariz={"@   J   @","{   \"   }","[   j   ]","<   -   >"};
-		String[] boca={"|  ===  |","|   -   |","|  ___  |","|  ---  |"};
-		String[] barbilla={"\\_______/","\\,,,,,,,/"};
-		
+		String[][] rasgos ={{"  pelo","WWWWWWWWW","\\\\\\//////","|\"\"\"\"\"\"\"|","|||||||||"},
+							{"  ojos","| O   O |","|-(· ·)-|","|-(o o)-|","| \\   / |"},
+							{"  nariz","@   J   @","{   \"   }","[   j   ]","<   -   >"},
+							{"  boca","|  ===  |","|   -   |","|  ___  |","|  ---  |"},
+							{"  barbilla","\\_______/","\\,,,,,,,/"} };
 		//Mostramos tipos
 		String espacios="     ";
 		System.out.println ("A continuación se mostrarán diferentes tipos de pelo, ojos, narices, bocas y barbillas para confeccionar el retrato robot \n\n");
-		for (int i=0;i<pelo.length;i++) {
-			if (i<barbilla.length) {
-				System.out.println((i+1)+". "+pelo[i]+espacios+(i+1)+". "+ojos[i]+espacios+(i+1)+". "+nariz[i]+espacios+(i+1)+". "+boca[i]+espacios+(i+1)+". "+barbilla[i]);
-			} else {
-				System.out.println((i+1)+". "+pelo[i]+espacios+(i+1)+". "+ojos[i]+espacios+(i+1)+". "+nariz[i]+espacios+(i+1)+". "+boca[i]);
+		int[] valorEscogido= new int[5];
+		for (int i=0;i<rasgos.length;i++) {
+			System.out.println("Elige un valor para el rasgo: "+rasgos[i][0]);
+			for (int j=1;j<rasgos[i].length;j++) {
+				System.out.println(espacios+j+"."+rasgos[i][j]);
 			}
+			System.out.println("\n\n");
+			valorEscogido[i]=Integer.parseInt(teclado.nextLine());
 		}
 		System.out.println("\n");
-		//Preguntamso por opciones a mostrar.
-		//Para completar el programa habría que realizar control de los datos que introduce el usuario.
-		System.out.print("Introduzca el código del pelo para el retrato robot: ");
-		int peloEscogido=teclado.nextInt();
-		System.out.print("Introduzca el código de los ojos para el retrato robot: ");
-		int ojosEscogido=teclado.nextInt();
-		System.out.print("Introduzca el código de la nariz para el retrato robot: ");
-		int narizEscogido=teclado.nextInt();
-		System.out.print("Introduzca el código de la boca para el retrato robot: ");
-		int bocaEscogido=teclado.nextInt();
-		System.out.print("Introduzca el código de la barbilla para el retrato robot: ");
-		int barbillaEscogido=teclado.nextInt();
 		
 		//Mostramos el retrato robot en función de las preferencias.
-		String mensaje="";
-		mensaje+=espacios+pelo[peloEscogido-1]+"\n"+espacios+ojos[ojosEscogido-1]+"\n"+espacios+nariz[narizEscogido-1]+"\n"+espacios+
-				boca[bocaEscogido-1]+"\n"+espacios+barbilla[barbillaEscogido-1];
-		System.out.println(mensaje);
-		
+		System.out.println("Este es el retrato robot escogido:\n\n");
+		for (int i=0;i<rasgos.length;i++) {
+			System.out.println(espacios+rasgos[i][valorEscogido[i]]);
+		}
 	}
 
 }
